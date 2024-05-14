d6561385859f1fbf4ba9cc24b967bd383f1e7017
Eric Charles <eric@datalayer.io>
2024-05-14 08:40:27 +0200

    fix: server extension

--- clouder/__init__.py
+++ clouder/__init__.py
@@ -3,18 +3,8 @@ from typing import Any, Dict, List
 from .serverapplication import ClouderServerApp
 
 
-__version__ = '0.0.6'
-
-
 def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
     return [{
         "module": "clouder",
         "app": ClouderServerApp,
-    }]
-
-
-def _jupyter_labextension_paths() -> List[Dict[str, str]]:
-    return [{
-        "src": "labextension",
-        "dest": "@datalayer/clouder"
     }]
