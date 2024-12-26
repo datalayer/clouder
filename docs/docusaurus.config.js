f4c350508138e8f51bc6affc7714b1032bc54a47
Eric Charles <eric@datalayer.io>
2024-12-26 15:03:31 +0100

    lint

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -38,28 +38,40 @@ module.exports = {
           label: 'Clouder',
         },
         {
-          href: 'https://www.linkedin.com/company/datalayer',
+          href: 'https://bsky.app/profile/datalayer.io',
           position: 'right',
-          className: 'header-linkedin-link',
-          'aria-label': 'Linkedin',
-        },
-        {
-          href: 'https://x.com/DatalayerIO',
-          position: 'right',
-          className: 'header-x-link',
-          'aria-label': 'X',
+          className: 'header-bluesky-link',
+          'aria-label': 'Bluesky',
         },
         {
           href: 'https://github.com/datalayer',
           position: 'right',
           className: 'header-github-link',
-          'aria-label': 'GitHub repository',
+          'aria-label': 'GitHub',
+        },
+        {
+          href: 'https://www.linkedin.com/company/datalayer',
+          position: 'right',
+          className: 'header-linkedin-link',
+          'aria-label': 'LinkedIn',
+        },
+        {
+          href: 'https://tiktok.com/@datalayerio',
+          position: 'right',
+          className: 'header-tiktok-link',
+          'aria-label': 'TikTok',
+        },
+        {
+          href: 'https://www.youtube.com/@datalayer',
+          position: 'right',
+          className: 'header-youtube-link',
+          'aria-label': 'YouTube',
         },
         {
           href: 'https://datalayer.io',
           position: 'right',
           className: 'header-datalayer-io-link',
-          'aria-label': 'Datalayer IO',
+          'aria-label': 'Datalayer',
         },
       ],
     },
@@ -79,16 +91,24 @@ module.exports = {
           title: 'Community',
           items: [
             {
+              label: 'Bluesky',
+              href: 'https://bsky.app/profile/datalayer.io',
+            },
+            {
               label: 'GitHub',
               href: 'https://github.com/datalayer',
             },
             {
-              label: 'X',
-              href: 'https://x.com/datalayerio',
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/datalayer',
             },
             {
-              label: 'Linkedin',
-              href: 'https://www.linkedin.com/company/datalayer',
+              label: 'TikTok',
+              href: 'https://tiktok.com/@datalayerio',
+            },
+            {
+              label: 'YouTube',
+              href: 'https://www.youtube.com/@datalayer',
             },
           ],
         },
@@ -96,16 +116,28 @@ module.exports = {
           title: 'More',
           items: [
             {
-              label: 'Datalayer IO',
+              label: 'Datalayer',
               href: 'https://datalayer.io',
+            },
+            {
+              label: 'Datalayer Docs',
+              href: 'https://docs.datalayer.io',
             },
             {
               label: 'Datalayer Tech',
               href: 'https://datalayer.tech',
             },
             {
+              label: 'Datalayer Guide',
+              href: 'https://datalayer.guide',
+            },
+            {
               label: 'Datalayer Blog',
               href: 'https://datalayer.blog',
+            },
+            {
+              label: 'Clouder',
+              href: 'https://clouder.sh',
             },
           ],
         },
